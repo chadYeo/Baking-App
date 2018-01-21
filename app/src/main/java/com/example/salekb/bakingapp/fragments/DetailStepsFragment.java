d43dbd96d8fc2509e2540698c41cbd9d31c5ed9e
Chad Yeo <chad.yeo.cy@gmail.com>
2018-01-21 00:58:26 -0500

    Added Media Seesion for External Media Control

--- app/src/main/java/com/example/salekb/bakingapp/fragments/DetailStepsFragment.java
+++ app/src/main/java/com/example/salekb/bakingapp/fragments/DetailStepsFragment.java
@@ -5,17 +5,15 @@ import android.app.Dialog;
 import android.app.LoaderManager;
 import android.content.Context;
 import android.content.Loader;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.media.session.MediaSessionCompat;
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,20 +23,24 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.salekb.bakingapp.R;
 import com.example.salekb.bakingapp.steps.Steps;
 import com.example.salekb.bakingapp.steps.StepsAdapter;
 import com.example.salekb.bakingapp.steps.StepsLoader;
 import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
@@ -50,7 +52,8 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DetailStepsFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<Steps>> {
+public class DetailStepsFragment extends Fragment
+        implements LoaderManager.LoaderCallbacks<List<Steps>>, ExoPlayer.EventListener {
 
     private static final int DETAIL_STEP_LOADER_ID = 4845;
     private ProgressBar mProgressbar;
@@ -65,13 +68,15 @@ public class DetailStepsFragment extends Fragment implements LoaderManager.Loade
     private FrameLayout mExoFullScreenButton;
     private ImageView mExoFullScreenIcon;
     private Dialog mFullScreenDialog;
+    private MediaSessionCompat mMediaSession;
+    private PlaybackStateCompat.Builder mStateBuilder;
 
     private boolean mIsExoPlayerFullscreen;
     private int stepsPosition;
     private int numberOfSteps;
 
     private static final String url = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/baking.json";
-    private static final String LOG_TAG = DetailStepsFragment.class.getSimpleName();
+    private static final String TAG = DetailStepsFragment.class.getSimpleName();
 
     public DetailStepsFragment() {
     }
@@ -160,12 +165,41 @@ public class DetailStepsFragment extends Fragment implements LoaderManager.Loade
             LoaderManager loaderManager = getActivity().getLoaderManager();
             loaderManager.initLoader(DETAIL_STEP_LOADER_ID, null, this);
 
-            Log.v(LOG_TAG, "loaderManager.initLoader is initiated");
+            Log.v(TAG, "loaderManager.initLoader is initiated");
         } else {
             mProgressbar.setVisibility(View.GONE);
         }
+        initializeMediaSession();
 
         return view;
+    }
+
+    /**
+      * Initializes the Media Session to be enabled with media buttons, transport controls, callbacks
+      * and media controller.
+     * */
+    private void initializeMediaSession() {
+        mMediaSession = new MediaSessionCompat(getContext(), TAG);
+
+        mMediaSession.setFlags(
+                MediaSessionCompat.FLAG_HANDLES_MEDIA_BUTTONS |
+                MediaSessionCompat.FLAG_HANDLES_TRANSPORT_CONTROLS);
+
+        // Do not let MediaButtons restart the player when the app is not visible
+        mMediaSession.setMediaButtonReceiver(null);
+
+        mStateBuilder = new PlaybackStateCompat.Builder()
+                .setActions(
+                        PlaybackStateCompat.ACTION_PLAY |
+                                PlaybackStateCompat.ACTION_PAUSE |
+                                PlaybackStateCompat.ACTION_SKIP_TO_PREVIOUS |
+                                PlaybackStateCompat.ACTION_PLAY_PAUSE);
+
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+
+        mMediaSession.setCallback(new MySessionCallback());
+
+        mMediaSession.setActive(true);
     }
 
     @Override
@@ -179,6 +213,7 @@ public class DetailStepsFragment extends Fragment implements LoaderManager.Loade
         if (!mStepsAdapter.steps.get(stepsPosition).getVideoURL().isEmpty()) {
             releasePlayer();
         }
+        mMediaSession.setActive(false);
     }
 
 
@@ -213,7 +248,7 @@ public class DetailStepsFragment extends Fragment implements LoaderManager.Loade
     @Override
     public void onLoaderReset(Loader<List<Steps>> loader) {
         loader.reset();
-        Log.v(LOG_TAG, "onLoaderReset is initiated");
+        Log.v(TAG, "onLoaderReset is initiated");
     }
 
     /**
@@ -232,6 +267,7 @@ public class DetailStepsFragment extends Fragment implements LoaderManager.Loade
                     getContext(), userAgent), new DefaultExtractorsFactory(), null, null);
             mExoPlayer.prepare(mediaSource);
             mExoPlayer.setPlayWhenReady(true);
+            mExoPlayer.addListener(this);
         }
     }
 
@@ -259,5 +295,63 @@ public class DetailStepsFragment extends Fragment implements LoaderManager.Loade
         mExoFullScreenIcon.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_fullscreen_shrink));
         mIsExoPlayerFullscreen = true;
         mFullScreenDialog.show();
+    }
+
+    // ExoPlayer Event Listener
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if ((playbackState == ExoPlayer.STATE_READY) && playWhenReady) {
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PLAYING,
+                    mExoPlayer.getCurrentPosition(), 1f);
+        } else if ((playbackState == ExoPlayer.STATE_READY)) {
+            mStateBuilder.setState(PlaybackStateCompat.STATE_PAUSED,
+                    mExoPlayer.getCurrentPosition(), 1f);
+        }
+        mMediaSession.setPlaybackState(mStateBuilder.build());
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity() {
+
+    }
+
+    /**
+      * Media Session Callbacks, where all external clients control the player.
+    */
+    private class MySessionCallback extends MediaSessionCompat.Callback {
+        @Override
+        public void onPlay() {
+            mExoPlayer.setPlayWhenReady(true);
+        }
+
+        @Override
+        public void onPause() {
+            mExoPlayer.setPlayWhenReady(false);
+        }
+
+        @Override
+        public void onSkipToPrevious() {
+            mExoPlayer.seekTo(0);
+        }
     }
 }
